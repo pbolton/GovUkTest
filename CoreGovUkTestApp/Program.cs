757e1fa3262388ee0d503ecc451f8b73b8925843
Peter Bolton <ptbolton@live.com>
2020-01-13 10:36:41 +0000

    Added Core Console App

---
+++ CoreGovUkTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using GovUkApi;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace CoreGovUkTestApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection().AddScoped<IGovUkApi, GovUkApi.GovUkApi>().BuildServiceProvider();
+
+            var api = serviceProvider.GetService<IGovUkApi>();
+
+            var users = await api.GetUsersInArea();
+        }
+    }
+}
